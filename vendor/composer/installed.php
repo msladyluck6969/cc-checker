b8bd129dd1244eb34628caf26cb22d5d22a3d774
msladyluck6969 <51029808+msladyluck6969@users.noreply.github.com>
2025-02-17 13:10:23 +0000

    committing

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c31cf4513bd8a5635555d9d23e7cbbc330803f80',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c31cf4513bd8a5635555d9d23e7cbbc330803f80',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
